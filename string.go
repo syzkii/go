d0121f146bf8bc69b31387b0b91ada465ce001f7
syzkii <rifkiardianakbar.raa@gmail.com>
2022-04-05 12:39:26 +0700

    update

---
+++ string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Rifki")
+	fmt.Println("Rifki Ardian")
+
+	fmt.Println(len("Rifki"))
+	fmt.Println("Rifki Ardian"[0])
+	fmt.Println("Rifki Ardian Akbar"[1])
+}
