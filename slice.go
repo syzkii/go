16bf9f164100157c0c5126ebbf98e3ab5dad5c60
syzkii <rifkiardianakbar.raa@gmail.com>
2022-04-18 17:24:56 +0700

    add: data

--- slice.go
+++ slice.go
@@ -54,4 +54,10 @@ func main() {
 	copy(copySlice, newSlice)
 	fmt.Println(copySlice)
 
+
+	iniArray := [...] int {1,2,3,4,5}
+	iniSlice := [] int {1,2,3,4,5}
+
+	fmt.Println(iniArray)
+	fmt.Println(iniSlice)
 }
